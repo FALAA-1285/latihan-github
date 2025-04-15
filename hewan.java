bcec603ec6a451ad899f75759c68913c63bedd53
muiddd <muidabdulmuid99@gmail.com>
2025-04-15 10:10:25 +0700

    pulen

--- hewan.java
+++ hewan.java
@@ -8,7 +8,17 @@ public class hewan{
 
     }
 
-    public hewan(String id, Sting nama, String jenis, String umur){
-        
+    public hewan(String id, String nama, String jenis, String umur){
+        this.id = id;
+        this.nama = nama;
+        this.jenis = jenis;
+        this.umur = umur;
     }
+
+    void tampil(){
+        System.out.println("Id    : " + id);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Jenis : " + jenis);
+        System.out.println("Umur  : " + umur);
+    } 
 }
