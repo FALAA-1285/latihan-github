e6453bcb079acd6e9765f8b0e1c01fdf240665f6
FALAA-1285 <ahmadfalahi1285@gmail.com>
2025-04-15 10:50:32 +0700

    panggil method

--- hewanMain.java
+++ hewanMain.java
@@ -2,40 +2,51 @@ import java.util.Scanner;
 
 public class hewanMain {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        hewan hewan[][]=new hewan[4][100];
-        do{
+        Scanner sc = new Scanner(System.in);
+        hewan hewan = new hewan();
+        dataHewan list = new dataHewan();
+        do {
             System.out.println("== Menu ==");
             System.out.println("1. Tambahkan Hewan");
             System.out.println("2. tampilkan Daftar hewan");
-            System.out.println("3. Urutkan berdasarkan jenis hewan");
+            System.out.println("3. Urutkan berdasarkan umur hewan");
             System.out.println("4. Cari Detail Hewan Dengan Id");
             System.out.println("5. Keluar");
             System.out.print("Masukkan Pilihan Menu: ");
             int pilihan = sc.nextInt();
-
+            sc.nextLine();
             switch (pilihan) {
                 case 1:
-                    
+                    System.out.print("Masukkan id hewan: ");
+                    String id = sc.nextLine();
+                    sc.nextLine();
+                    System.out.print("Masukkan Nama Hewan: ");
+                    String nama = sc.next();
+                    System.out.print("Masukkan Jenis Hewan: ");
+                    String jenis = sc.next();
+                    System.out.print("Masukkan Umur Hewan: ");
+                    int umur = sc.nextInt();
+                    list.tambah(new hewan(id, nama, jenis, umur));
                     break;
                 case 2:
-
+                    list.tampil();
                     break;
                 case 3:
-
+                    list.sortingASC();
                     break;
                 case 4:
-
-                    break;    
+                    System.out.print("Masukkan id yang dicari:");
+                    String cari = sc.nextLine();
+                    list.pencarianDataSequential(cari);
+                    break;
                 case 5:
-                System.out.println("Terima Kasih");
-                return;
+                    System.out.println("Terima Kasih");
+                    return;
                 default:
-                System.out.println("Pilihan Menu Tidak Tersedia");
+                    System.out.println("Pilihan Menu Tidak Tersedia");
                     break;
             }
-        }while(true);
+        } while (true);
 
-        
     }
 }
