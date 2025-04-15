a331aa695faa80fdcb168a0f9af8a01ba290b6fd
muiddd <muidabdulmuid99@gmail.com>
2025-04-15 10:05:03 +0700

    push meong

--- hewan.java
+++ hewan.java
@@ -1,3 +1,14 @@
 public class hewan{
-    
+    String id;
+    String nama;
+    String jenis;
+    String umur;
+
+    public hewan(){
+
+    }
+
+    public hewan(String id, Sting nama, String jenis, String umur){
+        
+    }
 }
