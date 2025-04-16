3247f0b69a30b98c2da5dd78fd5032ab4a3873b2
muiddd <muidabdulmuid99@gmail.com>
2025-04-16 08:22:44 +0700

    khlg

--- dataHewan.java
+++ dataHewan.java
@@ -1,113 +1,84 @@
 public class dataHewan {
-    hewan[] datahewan = new hewan[100];
+    hewan[] dataHewan = new hewan[100];
     int idx = 0;
 
-    // Tambah hewan ke array
     public void tambah(hewan hwn) {
-        if (idx < datahewan.length) {
-            datahewan[idx++] = hwn;
+        if (idx < dataHewan.length) {
+            dataHewan[idx++] = hwn;
         } else {
             System.out.println("Data sudah penuh!");
         }
     }
 
-    // Tampilkan semua hewan
     public void tampil() {
-        for (int i = 0; i < idx; i++) {
-            datahewan[i].tampil();
-        }
-    }
-
-    // Sorting berdasarkan umur (Ascending)
-    void sortingASC() {
-        for (int i = 0; i < idx; i++) {
-            for (int j = 1; j < idx - i; j++) {
-                if (datahewan[j - 1].umur > datahewan[j].umur) {
-                    hewan temp = datahewan[j];
-                    datahewan[j] = datahewan[j - 1];
-                    datahewan[j - 1] = temp;
-                }
+        if (idx == 0) {
+            System.out.println("Belum ada data hewan.");
+        } else {
+            for (int i = 0; i < idx; i++) {
+                dataHewan[i].tampil();
             }
         }
     }
 
-    // Sorting berdasarkan umur (Descending)
     public void sortingDSC() {
         for (int i = 0; i < idx - 1; i++) {
             int maxIdx = i;
             for (int j = i + 1; j < idx; j++) {
-                if (datahewan[j].umur > datahewan[maxIdx].umur) {
-                    maxIdx = j;
+                if (dataHewan[j].umur > dataHewan[maxIdx].umur) {
+                    maxIdx = j;;;
                 }
             }
-            hewan temp = datahewan[i];
-            datahewan[i] = datahewan[maxIdx];
-            datahewan[maxIdx] = temp;
+            hewan temp = dataHewan[i];
+            dataHewan[i] = dataHewan[maxIdx];
+            dataHewan[maxIdx] = temp;
         }
+        System.out.println("Data berhasil diurutkan berdasarkan umur (desc).");
     }
 
-    // Pencarian berdasarkan ID (Sequential Search)
     public void pencarianDataSequential(String id) {
-        hewan[] hasil = new hewan[idx];
-        int count = 0;
+        boolean ditemukan = false;
         for (int i = 0; i < idx; i++) {
-            if (datahewan[i] != null && datahewan[i].id.equalsIgnoreCase(id)) {
-                hasil[count++] = datahewan[i];
+            if (dataHewan[i] != null && dataHewan[i].id.equalsIgnoreCase(id)) {
+                System.out.println("hewan ditemukan:");
+                dataHewan[i].tampil();
+                ditemukan = true;
             }
         }
-
-        if (count > 0) {
-            System.out.println("Hewan ditemukan:");
-            for (int i = 0; i < count; i++) {
-                hasil[i].tampil();
-            }
-            if (count > 1) {
-                System.out.println("Peringatan: Ditemukan lebih dari satu hasil!");
-            }
-        } else {
-            System.out.println("Hewan tidak ditemukan.");
+        if (!ditemukan) {
+            System.out.println("hewan tidak ditemukan.");
         }
     }
 
-    // Pencarian berdasarkan umur (Binary Search)
     public void pencarianDataBinary(int umur) {
-        sortingASC();
+        sortingDSC();
+        int left = 0, right = idx - 1;
 
-        int left = 0, right = idx - 1, mid;
-        hewan[] hasil = new hewan[idx];
-        int count = 0;
-
+        boolean ditemukan = false;
         while (left <= right) {
-            mid = (left + right) / 2;
-            if (datahewan[mid].umur == umur) {
-                // cari ke kiri
+            int mid = (left + right) / 2;
+            if (dataHewan[mid].umur == umur) {
+                // Tampilkan semua hewan dengan umur yang sama
                 int temp = mid;
-                while (temp >= 0 && datahewan[temp].umur == umur) {
-                    hasil[count++] = datahewan[temp--];
+                while (temp >= 0 && dataHewan[temp].umur == umur) {
+                    dataHewan[temp].tampil();
+                    temp--;
                 }
-                // cari ke kanan
                 temp = mid + 1;
-                while (temp < idx && datahewan[temp].umur == umur) {
-                    hasil[count++] = datahewan[temp++];
+                while (temp < idx && dataHewan[temp].umur == umur) {
+                    dataHewan[temp].tampil();
+                    temp++;
                 }
+                ditemukan = true;
                 break;
-            } else if (datahewan[mid].umur < umur) {
+            } else if (dataHewan[mid].umur < umur) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
 
-        if (count > 0) {
-            System.out.println("Hewan ditemukan:");
-            for (int i = 0; i < count; i++) {
-                hasil[i].tampil();
-            }
-            if (count > 1) {
-                System.out.println("Ditemukan lebih dari satu hasil!");
-            }
-        } else {
-            System.out.println("Hewan tidak ditemukan.");
+        if (!ditemukan) {
+            System.out.println("hewan tidak ditemukan.");
         }
     }
 }
--- hewanMain.java
+++ hewanMain.java
@@ -2,45 +2,55 @@ import java.util.Scanner;
 
 public class hewanMain {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        hewan hewan[][]=new hewan[4][100];
-        do{
+        Scanner sc = new Scanner(System.in);
+        dataHewan list = new dataHewan();
+
+        System.out.println("aawokaowkoawkaokwoakwo");
+
+        do {
             System.out.println("== Menu ==");
             System.out.println("1. Tambahkan Hewan");
             System.out.println("2. tampilkan Daftar hewan");
-            System.out.println("3. Urutkan berdasarkan jenis hewan");
+            System.out.println("3. Urutkan berdasarkan umur hewan");
             System.out.println("4. Cari Detail Hewan Dengan Id");
             System.out.println("5. Keluar");
             System.out.print("Masukkan Pilihan Menu: ");
             int pilihan = sc.nextInt();
+            sc.nextLine();
 
             switch (pilihan) {
+                
                 case 1:
-                    
+                    System.out.print("Masukkan id hewan: ");
+                    String id = sc.nextLine();
+                    System.out.print("Masukkan Nama Hewan: ");
+                    String nama = sc.nextLine();
+                    System.out.print("Masukkan Jenis Hewan: ");
+                    String jenis = sc.nextLine();
+                    System.out.print("Masukkan Umur Hewan: ");
+                    int umur = sc.nextInt();
+                    sc.nextLine();
+                    list.tambah(new hewan(id, nama, jenis, umur));
                     break;
                 case 2:
-
+                    list.tampil();
                     break;
                 case 3:
-
+                    list.sortingDSC();
                     break;
                 case 4:
-
-                    break;    
+                    System.out.print("Masukkan id yang dicari:");
+                    String cari = sc.nextLine();
+                    list.pencarianDataSequential(cari);
+                    break;
                 case 5:
-                System.out.println("Terima Kasih");
-                return;
+                    System.out.println("Terima Kasih");
+                    return;
                 default:
-                System.out.println("Pilihan Menu Tidak Tersedia");
+                    System.out.println("Pilihan Menu Tidak Tersedia");
                     break;
             }
-        }while(true);
+        } while (true);
 
-
-
-
-
-
-        
     }
 }
